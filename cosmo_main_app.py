4f0b5fec7f0b56cd7c6c5f8412712462d9f058d3
布丁不打牌 <cosmo_tv@budingbudapaideMacBook-Pro.local>
2025-04-04 09:07:09 +0800

    上传宇宙映画 Flask 项目

---
+++ cosmo_main_app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+cosmo = Flask(__name__)
+
+@cosmo.route('/')
+def home():
+    return "Hello 这里是宇宙映画的第一个后端程序！"
+
+@cosmo.route('/submit', methods=['POST'])
+def receive():
+    data = request.get_json()
+    name = data.get('name')
+    phone = data.get('phone')
+    print(f"收到：{name} - {phone}")
+    return jsonify({"status": "received"})
+
+if __name__ == '__main__':
+    cosmo.run()
